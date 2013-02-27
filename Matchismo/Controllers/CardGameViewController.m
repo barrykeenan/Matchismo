6fddcdfab386fdd384abf0457cec1a1f17790528
keeny <barry@craftstudios.co.nz>
2013-02-27 21:34:13 +1300

    Wired up game model to controller. updateUI method.

--- Matchismo/Controllers/CardGameViewController.m
+++ Matchismo/Controllers/CardGameViewController.m
@@ -7,42 +7,58 @@
 //
 
 #import "CardGameViewController.h"
+#import "PlayingCard.h"
 #import "PlayingCardDeck.h"
+#import "CardMatchingGame.h"
 
 @interface CardGameViewController ()
-
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
-@property (nonatomic, strong) Deck *deck;
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
-
+@property (strong, nonatomic) CardMatchingGame *game;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
 @end
 
 @implementation CardGameViewController
 
-- (Deck *)deck
+- (CardMatchingGame *)game
 {
-    if (!_deck) _deck = [[PlayingCardDeck alloc] init];
-    return _deck;
+    if (!_game) _game = [[CardMatchingGame alloc] initWithCardCount:self.cardButtons.count
+                                                          usingDeck:[[PlayingCardDeck alloc] init]];
+    return _game;
 }
 
-- (void)setCardButtons:(NSArray *)cardButtons{
-	_cardButtons = cardButtons;
-	for (UIButton *cardButton in self.cardButtons) {
-		Card *card = [self.deck drawRandomCard];
-		[cardButton setTitle:card.contents forState:UIControlStateSelected];
-	}
+- (void)setCardButtons:(NSArray *)cardButtons
+{
+    _cardButtons = cardButtons;
+    [self updateUI];
 }
 
-- (IBAction)flipCard:(UIButton *)cardBtn {
-    cardBtn.selected = !cardBtn.selected;
-	self.flipCount++;
+- (void)updateUI
+{
+    for (UIButton *cardButton in self.cardButtons) {
+        Card *card = [self.game cardAtIndex:[self.cardButtons indexOfObject:cardButton]];
+        [cardButton setTitle:card.contents forState:UIControlStateSelected];
+        [cardButton setTitle:card.contents forState:UIControlStateSelected|UIControlStateDisabled];
+        cardButton.selected = card.isFaceUp;
+        cardButton.enabled = !card.isUnplayable;
+        cardButton.alpha = card.isUnplayable ? 0.3 : 1.0;
+    }
+    self.scoreLabel.text = [NSString stringWithFormat:@"Score: %d", self.game.score];
 }
 
-- (void)setFlipCount:(int)flipCount{
-	_flipCount = flipCount;
-	self.flipsLabel.text = [NSString stringWithFormat: @"Flips: %d", self.flipCount];
-	NSLog(@"flips updated to %d", self.flipCount);
+- (void)setFlipCount:(int)flipCount
+{
+    _flipCount = flipCount;
+    self.flipsLabel.text = [NSString stringWithFormat:@"Flips: %d", self.flipCount];
+    //NSLog(@"flips updated to %d", self.flipCount);
+}
+
+- (IBAction)flipCard:(UIButton *)sender
+{
+    [self.game flipCardAtIndex:[self.cardButtons indexOfObject:sender]];
+    self.flipCount++;
+    [self updateUI];
 }
 
 @end
