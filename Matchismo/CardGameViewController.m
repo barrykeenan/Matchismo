eef5e811aeab75b1d2ab552aa88e507dc7bf6457
keeny <barry@craftstudios.co.nz>
2013-02-27 19:55:24 +1300

    Random cards are done on setup, not on each flip

--- Matchismo/CardGameViewController.m
+++ Matchismo/CardGameViewController.m
@@ -7,12 +7,13 @@
 //
 
 #import "CardGameViewController.h"
+#import "PlayingCardDeck.h"
 
 @interface CardGameViewController ()
 
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
-@property (nonatomic, strong) PlayingCardDeck *deck;
+@property (nonatomic, strong) Deck *deck;
 @property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
 
 @end
@@ -25,18 +26,23 @@
     return _deck;
 }
 
-- (void)setFlipCount:(int)flipCount{
-	_flipCount = flipCount;
-	self.flipsLabel.text = [NSString stringWithFormat: @"Flips: %d", self.flipCount];
-	NSLog(@"flips updated to %d", self.flipCount);
+- (void)setCardButtons:(NSArray *)cardButtons{
+	_cardButtons = cardButtons;
+	for (UIButton *cardButton in self.cardButtons) {
+		Card *card = [self.deck drawRandomCard];
+		[cardButton setTitle:card.contents forState:UIControlStateSelected];
+	}
 }
 
 - (IBAction)flipCard:(UIButton *)cardBtn {
     cardBtn.selected = !cardBtn.selected;
-	
-	PlayingCard *randomCard = [self.deck drawRandomCard];
-	[cardBtn setTitle:randomCard.contents forState: UIControlStateSelected];
 	self.flipCount++;
+}
+
+- (void)setFlipCount:(int)flipCount{
+	_flipCount = flipCount;
+	self.flipsLabel.text = [NSString stringWithFormat: @"Flips: %d", self.flipCount];
+	NSLog(@"flips updated to %d", self.flipCount);
 }
 
 @end
