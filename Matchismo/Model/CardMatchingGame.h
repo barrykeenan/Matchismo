4496d216b265430970ee3ef2aabb6f3a88bf5edd
keeny <barry@craftstudios.co.nz>
2013-02-27 20:08:57 +1300

    Domain: New top level 'game'

---
+++ Matchismo/Model/CardMatchingGame.h
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.h
+//  Matchismo
+//
+//  Created by Barry Keenan on 27/02/13.
+//  Copyright (c) 2013 Craft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CardMatchingGame : NSObject
+
+@end
---
+++ Matchismo/Model/CardMatchingGame.m
@@ -0,0 +1,13 @@
+//
+//  CardMatchingGame.m
+//  Matchismo
+//
+//  Created by Barry Keenan on 27/02/13.
+//  Copyright (c) 2013 Craft. All rights reserved.
+//
+
+#import "CardMatchingGame.h"
+
+@implementation CardMatchingGame
+
+@end
