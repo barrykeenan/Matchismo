4e96258f4b25ded25efd1a1588596f3f1773d87b
keeny <barry@craftstudios.co.nz>
2013-02-27 21:09:18 +1300

    Game logic

--- Matchismo/Model/CardMatchingGame.h
+++ Matchismo/Model/CardMatchingGame.h
@@ -7,7 +7,18 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Deck.h"
+#import "Card.h"
 
 @interface CardMatchingGame : NSObject
+
+- (id) initWithCardCount:(NSUInteger)count
+			   usingDeck:(Deck *)deck;
+
+- (void)flipCardAtIndex:(NSUInteger)index;
+
+- (Card *)cardAtIndex:(NSUInteger)index;
+
+@property(readonly, nonatomic) int score;
 
 @end
