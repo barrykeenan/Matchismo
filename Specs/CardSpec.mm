8ad370974b5a80dd17ec76a5d5159f977156eb3e
keeny <barry@craftstudios.co.nz>
2013-03-14 13:09:19 +1300

    doesnt match another card with different contents

--- Specs/CardSpec.mm
+++ Specs/CardSpec.mm
@@ -16,6 +16,25 @@ describe(@"Card", ^{
 	it(@"returns human readable contents", ^{
         sut.contents should equal(@"ace of spades");
     });
+	
+	it(@"matches another card with the same contents", ^{
+		Card *another_card = [[Card alloc] init];
+		another_card.contents = @"ace of spades";
+		
+		int score = [sut match:@[another_card]];
+		
+        score should equal(1);
+    });
+	
+	it(@"doesnt match another card with different contents", ^{
+		Card *another_card = [[Card alloc] init];
+		another_card.contents = @"queen of diamonds";
+		
+		int score = [sut match:@[another_card]];
+		
+        score should equal(0);
+    });
+
 });
 
 SPEC_END
