b9a6af113b910c6a65bc07f37ca38fa22fda7857
keeny <barry@craftstudios.co.nz>
2013-03-14 13:12:15 +1300

    Removed old test project

---
+++ Specs/PlayingCardSpec.mm
@@ -0,0 +1,16 @@
+#import "PlayingCard.h"
+
+using namespace Cedar::Matchers;
+using namespace Cedar::Doubles;
+
+SPEC_BEGIN(PlayingCardSpec)
+
+describe(@"PlayingCard", ^{
+    __block PlayingCard *model;
+
+    beforeEach(^{
+
+    });
+});
+
+SPEC_END
